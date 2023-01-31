924de72b973f6f3d5aae095f3334eb598026b347
peterlits zo <peterlitszo@outlook.com>
2023-01-31 20:49:30 +0800

    feat: Not world

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Hello, world.
+        Hello, not world.
       </main>
     </>
   )
